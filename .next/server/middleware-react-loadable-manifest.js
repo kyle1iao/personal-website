36f61a4c8952545147586c3679604f7954e0b174
Kyle Liao <kyleliao@sas.upenn.edu>
2023-06-30 22:33:15 -0400

    Ignore .next directory

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"..\\\\components\\\\layouts\\\\main.js -> ../voxel-art.js\":{\"id\":\"..\\\\components\\\\layouts\\\\main.js -> ../voxel-art.js\",\"files\":[\"static/chunks/components_voxel-art_js.js\"]}}"
