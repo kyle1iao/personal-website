9dd48e36698e5b06dbd5f12d7430c9e108437d1f
Kyle Liao <kyleliao@sas.upenn.edu>
2023-06-30 21:34:42 -0400

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import theme from '../lib/theme'
 import Fonts from '../components/fonts'
--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,4 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-// import Layout from '../components/layouts/article'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
